08c325155259eb552aff742425bce600a611e0e9
Zech Xu <zhenjiang.xu@gmail.com>
2021-04-25 00:47:11 +0800

    delete recipe.el

--- recipe.el
+++
@@ -1,6 +0,0 @@
-;; this is for submission to melpa package repo:
-;; https://github.com/milkypostman/melpa
-(seqel :fetcher github
-       :repo "rnaer/seqel"
-       :files ("*.el" "test/*"
-               "README.org"))
